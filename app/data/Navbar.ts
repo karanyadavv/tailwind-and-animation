430c7586ba7d5ea185a3675a1acc69a95c348440
karanyadavv <karanyadav729@gmail.com>
2025-09-28 03:18:18 +0530

    added navbar

---
+++ app/data/Navbar.ts
@@ -0,0 +1,22 @@
+export const NavBarLinks = [
+  {
+    name: "Products",
+    href: "#",
+  },
+  {
+    name: "Solutions",
+    href: "#",
+  },
+  {
+    name: "Developers",
+    href: "#",
+  },
+  {
+    name: "Resources",
+    href: "#",
+  },
+  {
+    name: "Pricing",
+    href: "#",
+  },
+];
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,11 +1,26 @@
 import Image from "next/image";
 import React from "react";
 import Logo from "../public/vercel.svg";
+import { NavBarLinks } from "@/app/data/Navbar";
+import Link from "next/link";
 
 const NavBar = () => {
   return (
-    <div className="shadow-navbar mx-auto mt-4 flex max-w-xl items-center justify-between rounded-2xl border border-neutral-200 p-4">
+    <div className="shadow-navbar mx-auto mt-4 flex max-w-4xl items-center justify-between rounded-2xl border border-neutral-500 p-4">
       <Image alt="logo" src={Logo} height={50} width={50} className="" />
+      <div className="mr-10 flex items-center gap-4 text-sm text-neutral-500">
+        {NavBarLinks.map((link, index) => {
+          return (
+            <Link
+              className="hover:text-neutral-200"
+              href={link.href}
+              key={index}
+            >
+              {link.name}
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 };
