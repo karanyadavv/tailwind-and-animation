3bad4e6a9263691c28f75de86fcf4b00c129ab62
karanyadavv <karanyadav729@gmail.com>
2025-10-05 22:49:32 +0530

    made herosection responsive

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -3,17 +3,17 @@ import React from "react";
 const HeroSection = () => {
   return (
     <div className="my-20 flex w-full flex-col items-center justify-center">
-      <h1 className="max-w-2xl bg-gradient-to-b from-neutral-50 to-neutral-600 bg-clip-text text-center text-7xl leading-tight font-bold tracking-tight text-transparent">
+      <h1 className="max-w-2xl bg-gradient-to-b from-neutral-50 to-neutral-600 bg-clip-text text-center text-4xl leading-tight font-bold tracking-tight text-transparent md:text-7xl">
         Unleash the power of intuitive finance
       </h1>
-      <p className="mx-auto mt-10 max-w-3xl text-center text-xl text-neutral-500">
+      <p className="mx-auto mt-10 max-w-3xl text-center text-sm text-neutral-500 md:text-xl">
         Say goodbye to the <span className="text-primary"> outdated</span>{" "}
         financial tools. Every small business owner, regardless of the
         background, can now manage their{" "}
         <span className="text-primary"> business</span> like a pro. Simple.
         Intuitive. And never boring.
       </p>
-      <div className="mt-5 flex w-full max-w-lg">
+      <div className="mt-5 flex w-full max-w-sm md:max-w-lg">
         <input
           type="text"
           className="focus:ring-primary mr-4 flex-1 rounded-xl border border-neutral-600 px-4 text-white transition duration-200 placeholder:text-neutral-500 focus:ring-1 focus:outline-none"
@@ -25,7 +25,7 @@ const HeroSection = () => {
         </button>
       </div>
       <div className="group mt-6 flex items-center justify-center gap-1.5">
-        <button className="inline-flex cursor-pointer rounded-xl px-4 py-2 transition-all duration-200 hover:bg-neutral-800">
+        <button className="inline-flex cursor-pointer rounded-xl px-4 py-2 transition-all duration-200 hover:bg-neutral-800 active:scale-97">
           <span className="bg-gradient-to-r from-neutral-50 to-neutral-600 bg-clip-text font-medium text-transparent">
             New: Product Intelligence
           </span>
