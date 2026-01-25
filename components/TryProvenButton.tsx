056c478902a4301674781ccf006f516ea2f76049
karanyadavv <karanyadav729@gmail.com>
2026-01-25 16:52:15 +0530

    fixed motion bugs

--- components/TryProvenButton.tsx
+++ components/TryProvenButton.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { motion } from "motion/react";
 
+const buttonVariant = {
+  rest: {},
+  hover: { scale: 0.92 },
+};
+
 const firstText = {
   rest: {
     y: 0,
-    transition: {
-      type: "spring",
-      duration: 0.3,
-      bounce: 0,
-    },
   },
   hover: {
     y: "-100%",
@@ -18,19 +18,9 @@ const firstText = {
 const secondText = {
   rest: {
     y: "100%",
-    transition: {
-      type: "spring",
-      duration: 0.3,
-      bounce: 0,
-    },
   },
   hover: {
     y: 0,
-    // transition: {
-    //   type: "spring",
-    //   duration: 0.3,
-    //   bounce: 0,
-    // },
   },
 };
 
@@ -38,10 +28,13 @@ const TryProvenButton = () => {
   return (
     <div className="my-24">
       <motion.button
+        initial="false"
         whileHover={"hover"}
-        className="group font-google cursor-pointer overflow-hidden rounded-4xl bg-[#131515] px-6 py-2.5 text-white shadow-[0px_5px_20px_rgba(0,0,0,0.70)] transition-transform duration-300 hover:scale-92"
+        variants={buttonVariant}
+        transition={{ type: "spring", duration: 0.3, bounce: 0 }}
+        className="group font-google cursor-pointer overflow-hidden rounded-4xl bg-[#131515] px-6 py-2.5 text-white shadow-[0px_5px_20px_rgba(0,0,0,0.70)]"
       >
-        <motion.div className="relative flex flex-col items-center justify-center overflow-hidden">
+        <div className="relative flex flex-col items-center justify-center overflow-hidden">
           <motion.span variants={firstText} className="">
             See how suggestions work
           </motion.span>
@@ -51,7 +44,7 @@ const TryProvenButton = () => {
           >
             See how suggestions work
           </motion.span>
-        </motion.div>
+        </div>
       </motion.button>
     </div>
   );
