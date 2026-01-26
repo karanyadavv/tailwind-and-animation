a79815650da7f4d4c4265950c789b0e3e6ab1336
karanyadavv <karanyadav729@gmail.com>
2026-01-26 16:54:43 +0530

    fixed variants for 2nd button finally

--- components/TryProvenButton.tsx
+++ components/TryProvenButton.tsx
@@ -9,18 +9,24 @@ const buttonVariant = {
 const firstText = {
   rest: {
     y: 0,
+    opacity: 1,
+    rotateX: 0,
   },
   hover: {
     y: "-100%",
+    opacity: 0,
+    rotateX: -90,
   },
 };
 
 const secondText = {
   rest: {
     y: "100%",
+    opacity: 0,
   },
   hover: {
     y: 0,
+    opacity: 1,
   },
 };
 
@@ -28,19 +34,24 @@ const TryProvenButton = () => {
   return (
     <div className="my-24">
       <motion.button
-        initial="false"
-        whileHover={"hover"}
+        initial="rest"
+        whileHover="hover"
         variants={buttonVariant}
         transition={{ type: "spring", duration: 0.3, bounce: 0 }}
-        className="group font-google cursor-pointer overflow-hidden rounded-4xl bg-[#131515] px-6 py-2.5 text-white shadow-[0px_5px_20px_rgba(0,0,0,0.70)]"
+        className="font-google cursor-pointer overflow-hidden rounded-4xl bg-[#131515] px-6 py-2.5 text-white shadow-[0px_5px_20px_rgba(0,0,0,0.70)]"
       >
         <div className="relative flex flex-col items-center justify-center overflow-hidden">
-          <motion.span variants={firstText} className="">
+          <motion.span
+            variants={firstText}
+            transition={{ type: "spring", duration: 0.3, bounce: 0.1 }}
+            className=""
+          >
             See how suggestions work
           </motion.span>
           <motion.span
             variants={secondText}
-            className="absolute translate-y-full transition-transform duration-300 group-hover:translate-y-0"
+            transition={{ type: "spring", duration: 0.3, bounce: 0.1 }}
+            className="absolute"
           >
             See how suggestions work
           </motion.span>
